737df68a32517d6dfe53bb1f7c80fa268e13f276
Fake1ove <teebenten10@gmail.com>
2025-04-12 03:35:39 +0700

    Update _getdatasheet.php

--- _getdatasheet.php
+++ _getdatasheet.php
@@ -5,8 +5,8 @@
           $curl = curl_init();
 
           curl_setopt_array($curl, array(
-            // CURLOPT_URL => 'https://script.google.com/macros/s/AKfycbyzdu4F8htnwiuxV_gGo46Fonjle7Ha34PLD7jeaiG9ZOBa4jBtPF6LCdVi9ckMd5hT/exec?action=getUsers',
-            CURLOPT_URL => 'https://script.google.com/macros/s/AKfycbxqawsXAcCZfqVeqknK39OU-WazCS0juCApUD4mOsfjDfReY76vV6DglU36e3KE4wOOLQ/exec?action=getUsers',
+          
+            CURLOPT_URL => 'https://script.google.com/macros/s/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx?action=getUsers',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
